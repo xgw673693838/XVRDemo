cccabcc9d32c0ae703f06709eb3ea17a7cd9ea5e
xgw673693838 <weige@weigedeimac.local>
2016-10-24 15:24:49 +0800

    Initial Commit

---
+++ XVRDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XVRDemo
+//
+//  Created by weige on 16/10/24.
+//  Copyright © 2016年 Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XVRDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XVRDemo
+//
+//  Created by weige on 16/10/24.
+//  Copyright © 2016年 Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
